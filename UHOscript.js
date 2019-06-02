ba7d55205a16ad458be44d43d32627b78ff1fb67
Liryax <jo43pml@hotmail.fr>
2019-06-02 22:37:29 +0200

    Create UHOscript.js

---
+++ UHOscript.js
@@ -0,0 +1,50 @@
+// ==UserScript==
+// @name         Undercads : Hide Opponent
+// @match        https://*.undercards.net/Game
+// @exclude      https://*.undercards.net/Play
+// @version      0.1
+// @description  try to take over the world!
+// @author       Brittle Dread
+// @match        https://undercards.net/Quests
+// @grant        none
+// ==/UserScript==
+
+
+
+document.getElementById("enemyUsername").innerHTML = "Opponent"
+document.getElementById("enemyLevel").innerHTML = "???"
+
+var UHOrealName = ""
+var UHOrealLV = ""
+
+function UHOrevealInfo(){
+    document.getElementById("enemyUsername").innerHTML = UHOrealName
+    document.getElementById("enemyLevel").innerHTML = UHOrealLV
+}
+
+function UHOhideInfo(){
+    document.getElementById("enemyUsername").innerHTML = "Opponent"
+    document.getElementById("enemyLevel").innerHTML = "???"
+}
+
+
+
+var UHOintervalId = setInterval(function(){
+
+    if(document.getElementById("enemyLevel").innerHTML != "???"){
+        UHOrealName = document.getElementById("enemyUsername").innerHTML
+        UHOrealLV = document.getElementById("enemyLevel").innerHTML
+
+        UHOhideInfo()
+
+        document.getElementById("enemyUsername").addEventListener("mouseover",UHOrevealInfo)
+        document.getElementById("enemyUsername").addEventListener("mouseout",UHOhideInfo)
+
+        document.getElementById("enemyLevel").addEventListener("mouseover",UHOrevealInfo)
+        document.getElementById("enemyLevel").addEventListener("mouseout",UHOhideInfo)
+
+        clearInterval(UHOintervalId);
+    }
+
+
+}, 100);
